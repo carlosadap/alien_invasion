5f7841570ad9a84bb807c7ee0c81730214e2b303
Carlos Pinheiro <carlosadap@gmail.com>
2021-09-13 19:04:48 +0200

    Scale the points with the level

--- settings.py
+++ settings.py
@@ -21,7 +21,10 @@ class Settings:
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.2
+
+        # How quicly the alien point values increase
+        self.scora_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -38,9 +41,11 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
 
 
